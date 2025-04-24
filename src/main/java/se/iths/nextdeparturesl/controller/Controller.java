7d739eebecb8474026113fcb99b97618b8ffb1b2
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-04-24 11:05:48 +0200

    Added RestController to call the serach service and getting departures.

---
+++ src/main/java/se/iths/nextdeparturesl/controller/Controller.java
@@ -0,0 +1,25 @@
+package se.iths.nextdeparturesl.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import se.iths.nextdeparturesl.service.SearchService;
+
+@RestController
+public class Controller {
+    private final SearchService searchService = new SearchService();
+
+    @GetMapping("/stationList")
+    public ResponseEntity<?> stationList() {
+        System.out.println("hämtar lista");
+        return ResponseEntity.ok().body(searchService.getStationList());
+    }
+
+    @GetMapping("/searchStation/Barkarby")
+    public ResponseEntity<?> searchStation(@RequestParam String stationName) {
+        System.out.println("söker station: " + stationName);
+        return ResponseEntity.ok().body(searchService.departures(stationName));
+    }
+}
