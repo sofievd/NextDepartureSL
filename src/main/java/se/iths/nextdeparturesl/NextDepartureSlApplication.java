ad8e154d7e7063aa3e9a1e7dea0f6c42c88e3c61
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-04-24 10:19:10 +0200

    initial commit

---
+++ src/main/java/se/iths/nextdeparturesl/NextDepartureSlApplication.java
@@ -0,0 +1,13 @@
+package se.iths.nextdeparturesl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextDepartureSlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextDepartureSlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/iths/nextdeparturesl/NextDepartureSlApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.nextdeparturesl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextDepartureSlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
