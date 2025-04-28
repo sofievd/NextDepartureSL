b50ba355158e27ec36d2bc2068a7632b6a416e12
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-04-28 10:40:39 +0200

    Added a vehicle converter class that will convert a string of numbers to the correct vehicle type sucha s buss, railway, etc

---
+++ src/main/java/se/iths/nextdeparturesl/VehicleTypeConverter.java
@@ -0,0 +1,28 @@
+package se.iths.nextdeparturesl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VehicleTypeConverter {
+    private Map<String, String> vehicleCodeToString = new HashMap<String, String>();
+
+    public VehicleTypeConverter() {
+        vehicleCodeToString.put("100", "Railway");
+        vehicleCodeToString.put("101", "High Speed Rail");
+        vehicleCodeToString.put("102", "Long Distance Rail");
+        vehicleCodeToString.put("103", "Inter Regional Rail");
+        vehicleCodeToString.put("105", "Sleeper Rail");
+        vehicleCodeToString.put("106", "Regional Rai");
+        vehicleCodeToString.put("400", "Metro");
+        vehicleCodeToString.put("700", "Buss");
+        vehicleCodeToString.put("714", "Rail Replacement Bus");
+        vehicleCodeToString.put("900", "Tram");
+        vehicleCodeToString.put("1000", "Water Transport");
+        vehicleCodeToString.put("1501", "Communal Taxi");
+    }
+
+    public String convert(String vehicleCode) {
+        return vehicleCodeToString.get(vehicleCode);
+    }
+
+}
