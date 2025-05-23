a7a5a75e66283a0b78d70b1daf217b4d01f9dc06
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-05-23 18:25:00 +0200

    refaktioring

--- src/main/java/se/iths/nextdeparturesl/view/Departure.java
+++ src/main/java/se/iths/nextdeparturesl/view/Departure.java
@@ -10,8 +10,9 @@ public class Departure {
     private String routeDescription;
     private String lineNumber;
 
-    public Departure(String destination, String departureTime,String arrivalTime, String vehicleType,String vehicleTypeCode, String routeName, String routeDescription,
-            String lineNumber) {
+    public Departure(String destination, String departureTime,String arrivalTime,
+                     String vehicleType,String vehicleTypeCode, String routeName,
+                     String routeDescription, String lineNumber) {
         this.destination = destination;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
@@ -38,65 +39,8 @@ public class Departure {
     public String getDestination() {
         return destination;
     }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public String getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(String departureTime) {
-        this.departureTime = departureTime;
-    }
-
     public String getVehicleType() {
         return vehicleType;
-    }
-
-    public void setVehicleType(String vehicleType) {
-        this.vehicleType = vehicleType;
-    }
-
-    public String getLineNumber() {
-        return lineNumber;
-    }
-
-    public void setLineNumber(String lineNumber) {
-        this.lineNumber = lineNumber;
-    }
-
-    public String getArrivalTime() {
-        return arrivalTime;
-    }
-
-    public void setArrivalTime(String arrivalTime) {
-        this.arrivalTime = arrivalTime;
-    }
-
-    public String getRouteName() {
-        return routeName;
-    }
-
-    public void setRouteName(String routeName) {
-        this.routeName = routeName;
-    }
-
-    public String getRouteDescription() {
-        return routeDescription;
-    }
-
-    public void setRouteDescription(String routeDescription) {
-        this.routeDescription = routeDescription;
-    }
-
-    public String getVehicleTypeCode() {
-        return vehicleTypeCode;
-    }
-
-    public void setVehicleTypeCode(String vehicleTypeCode) {
-        this.vehicleTypeCode = vehicleTypeCode;
     }
 
 }
