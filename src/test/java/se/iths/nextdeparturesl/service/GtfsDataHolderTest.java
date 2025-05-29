7897e37a62dbe96b98322e3267bb1326a2c32475
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-05-29 18:56:18 +0200

    renamed MapServiceTest to GtfsDataHolderTest

--- src/test/java/se/iths/nextdeparturesl/service/GtfsDataHolderTest.java
+++ src/test/java/se/iths/nextdeparturesl/service/GtfsDataHolderTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MapServiceTest {
+class GtfsDataHolderTest {
     private GtfsDataHolder gtfsDataHolder;
 
     @BeforeEach
