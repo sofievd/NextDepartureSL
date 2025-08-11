76c01b16e928bc080df6b46d47727e60e74922d3
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-08-11 17:29:15 +0200

    updated controller to send out the vehicle positions.

--- src/main/java/se/iths/nextdeparturesl/controller/Controller.java
+++ src/main/java/se/iths/nextdeparturesl/controller/Controller.java
@@ -8,16 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import se.iths.nextdeparturesl.service.DepartureFinder;
-import se.iths.nextdeparturesl.util.ApiDownloader;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+
 
 @CrossOrigin("*")
 @RestController
 public class Controller {
     private static final Logger log = LogManager.getLogger();
     private final DepartureFinder departureFinder = new DepartureFinder();
+    //private final VehicleService vehicleService = new VehicleService();
     public Controller(){
 departureFinder.setUp();
     }
@@ -27,21 +27,27 @@ departureFinder.setUp();
        log.info("getting station list");
         return ResponseEntity.ok().body(departureFinder.getStationList());
     }
-    @GetMapping("/download")
-    public ResponseEntity<?> download() {
-        System.out.println("here");
-        ApiDownloader download = new ApiDownloader();
-        LocalDateTime now = LocalDateTime.now();
-        String today = now.format(DateTimeFormatter.ofPattern(("YYYY-MM-dd")));
-      //  download.download(today);
-        return ResponseEntity.ok().body("download completed");
 
-    }
+//    @GetMapping("/download")
+//    public ResponseEntity<?> download() {
+//        System.out.println("here");
+//        ApiDownloader download = new ApiDownloader();
+//        LocalDateTime now = LocalDateTime.now();
+//        String today = now.format(DateTimeFormatter.ofPattern(("YYYY-MM-dd")));
+//      //  download.download(today);
+//        return ResponseEntity.ok().body("download completed");
+//    }
 
     @GetMapping("/searchStation")
     public ResponseEntity<?> searchStation(@RequestParam String stationName) {
         log.info("searching station: {}", stationName);
         LocalDateTime now = LocalDateTime.now();
         return ResponseEntity.ok().body(departureFinder.getDeparturesFromStopName(stationName,now));
+    }
+
+    @GetMapping("/vehiclePositions")
+    public ResponseEntity<?> vehiclePosition(){
+        log.info("getting vehicle position");
+        return ResponseEntity.ok().body(departureFinder.getVehicles());
     }
 }
