10b4da3c38cf52cce88ef5e3699d5930fc5a49ae
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-09-22 10:37:24 +0200

    Added test class for the realtime downloader class

---
+++ src/test/java/se/iths/nextdeparturesl/util/GtfsRealtimeDownloadTaskTest.java
@@ -0,0 +1,13 @@
+package se.iths.nextdeparturesl.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GtfsRealtimeDownloadTaskTest {
+
+    @Test
+    void run() {
+
+    }
+}
