ff61e839e42f4e95a62fb17e3bc90af3bd8e5c4c
Sofie Van Dingenen <sofievandingenen@hotmail.com>
2025-10-29 17:22:36 +0100

    Added Station dto that contains an id and a name.

---
+++ src/main/java/se/iths/nextdeparturesl/dto/Station.java
@@ -0,0 +1,27 @@
+package se.iths.nextdeparturesl.dto;
+
+public class Station {
+    private String id;
+    private String name;
+
+    public Station(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
